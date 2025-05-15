99635e9454f4e272a082bcd7b4cf3e182ce63c67
Hernan Olmeda <holmedamartin@gmail.com>
2025-05-15 19:55:48 +0200

    cambio de la carpetra resources

--- api/index.js
+++ api/index.js
@@ -2,13 +2,11 @@ import { join } from 'path';
 import { readFileSync } from 'fs';
 
 export default function handler(req, res) {
-    // Solo permitimos GET
     if (req.method !== 'GET') {
         res.status(405).send('Método no permitido');
         return;
     }
 
-    // Enviar el index.html
     const filePath = join(process.cwd(), 'public', 'index.html');
     const html = readFileSync(filePath, 'utf8');
 
