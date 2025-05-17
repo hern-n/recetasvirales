9b98f590b35a8f52c61acca516d8169f0980447b
Hernan Olmeda <holmedamartin@gmail.com>
2025-05-17 20:30:55 +0200

    2

--- api/database.js
+++ api/database.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
                 return res.status(404).json({ error: 'Receta no encontrada por name' });
             }
         } else if (category) {
-            result = await db.sql`SELECT * FROM recetas WHERE titulo LIKE ${'%' + category + '%'};`;
+            result = await db.sql`SELECT * FROM recetas WHERE categoria LIKE ${'%' + category + '%'};`;
             if (result.length === 0) {
                 return res.status(404).json({ error: 'No se encontraron recetas en esa categoría' });
             }
