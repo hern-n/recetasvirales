7196df94a0039b6e86c8472120b34484b84c4328
Hernan Olmeda <holmedamartin@gmail.com>
2025-05-30 10:55:09 +0200

    cambiar resources

--- public/functions.js
+++ public/functions.js
@@ -15,7 +15,7 @@ export function createTaskBar() {
     };
 
     const logoImg = document.createElement("img");
-    logoImg.src = "/MainPage/resources/logo.png";
+    logoImg.src = "/resources/logo.png";
     logoImg.alt = "Logo";
     logo.appendChild(logoImg);
     headerTop.appendChild(logo);
