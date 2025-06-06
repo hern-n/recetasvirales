712daf7b96db40dbd6a93e34e9973f912fb9bd67
Hernan Olmeda <holmedamartin@gmail.com>
2025-06-06 16:44:44 +0200

    2

--- api/supabase.js
+++ api/supabase.js
@@ -1,6 +1,10 @@
 import postgres from 'postgres';
 
-const connectionString = process.env.DATABASE_URL;
+
+const DbUrl = "postgresql://postgres.pxglljjronhffzppedqt:Githubhernan1@aws-0-eu-north-1.pooler.supabase.com:5432/postgres"
+
+
+const connectionString = DbUrl;
 
 if (!connectionString) {
     throw new Error('Falta la variable de entorno DATABASE_URL');
