ba0bb256d433330dae9209a22ca9cecbdec11726
Hernan <holmedamartin@gmail.com>
2026-06-20 19:18:14 +0200

    refactor: move hardcoded database URL to env variable

--- api/supabase.js
+++ api/supabase.js
@@ -1,10 +1,8 @@
+import 'dotenv/config';
 import postgres from 'postgres';
 
 
-const DbUrl = "postgresql://postgres.pxglljjronhffzppedqt:Githubhernan1@aws-0-eu-north-1.pooler.supabase.com:5432/postgres"
-
-
-const connectionString = DbUrl;
+const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
     throw new Error('Falta la variable de entorno DATABASE_URL');
